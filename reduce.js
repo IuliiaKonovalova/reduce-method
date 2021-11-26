b58bc74e58dddf836f540f16ccd020a30a7fe669
Iuliia_Konovalova <JuliiaKonn@protonmail.com>
2021-11-26 06:19:44 +0000

    Add solution to totaling specific object property

--- reduce.js
+++ reduce.js
@@ -49,5 +49,19 @@ const teamMembers = [
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience)
 
 // Grouping by a property, and totaling it too
+
+let groupByProfession = teamMembers.reduce((acc, curr) =>{
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc
+}, {})
+
+console.log(groupByProfession)
